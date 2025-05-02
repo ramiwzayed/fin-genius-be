c0625ea5c12dabeacf3365bf3b133ae8be1f3369
ramiwzayed <ramiwzayed@gmail.com>
2025-05-02 22:47:50 +0200

    Disable UserModule and related code
    Commented out UserModule and its associated logic, including services, controllers, and entity imports. This might be a preparation step for refactoring, temporary deactivation, or pending removal.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
+
+// import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -16,7 +17,7 @@ import { UserModule } from './user/user.module';
       entities: [],
       synchronize: true,
     }),
-    UserModule,
+    // UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
